eaa0ef3f29be387d4ef0a0174f0ad8f60ea4269a
HvyD <zscottywd@msn.com>
2020-06-13 23:38:51 -0700

    initial add

--- JavaScript Test Lab/ducktypium.js
+++ JavaScript Test Lab/ducktypium.js
@@ -1,10 +1,55 @@
 class Ducktypium {
   constructor(color) {
     // your code here
+    this.color = color;
+    this.calibrationSequence = [];
   }
 
   // your code here
+  refract(addColor){
+
+    try {
+    refract(addColor)
+    }
+    catch(e) {
+      if(this.color != 'red'){
+          throw new SyntaxError("Only Red, Green, Yellow!!");
+        }
+      if(this.color != 'green'){
+        throw new SyntaxError("Only Red, Green, Yellow!!");
+      }
+      if(this.color != 'yellow'){
+        throw new SyntaxError("Only Red, Green, Yellow!!");
+      }
+
+    }
+    finally {
+      if(this.color === ('red') && addColor === ('blue')) {
+        return 'purple';
+      }
+      else if (this.color === ('red') && addColor === ('yellow')){
+        return 'orange';
+      }
+      else if(this.color === ('yellow') && addColor === ('blue')){
+        return 'green';
+      }
+      else{return addColor};
+    }
+
+
 }
+
+
+    calibrate(arr) {
+      arr.sort();
+      this.calibrationSequence = arr.map(function(x) { return x * 3; });
+      return this.calibrationSequence;
+
+
+  }
+}
+
+
 
 // The following lines of code are not required for the solution, but can be
 // used by you to test your solution.
